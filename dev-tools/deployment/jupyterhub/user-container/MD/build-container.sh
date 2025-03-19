4993345fb9f05af0bbcbb40160410e61e62334a8
lahiruj <lahirujayathilake@gmail.com>
2025-03-19 02:01:28 -0400

    updated the jupyter notebook base image to support ro data and wr code

--- dev-tools/deployment/jupyterhub/user-container/MD/build-container.sh
+++ dev-tools/deployment/jupyterhub/user-container/MD/build-container.sh
@@ -1,2 +1,2 @@
-docker build --platform linux/x86_64 -t cybershuttle/md-user-notebook .
-docker push cybershuttle/md-user-notebook
+docker build --platform linux/x86_64 -t cybershuttle/jupyterlab-base .
+docker push cybershuttle/jupyterlab-base
--- dev-tools/deployment/jupyterhub/user-container/MD/init.sh
+++ dev-tools/deployment/jupyterhub/user-container/MD/init.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
 TARGET_DIR="/home/jovyan/work"
+SHARED_TMP="/home/jovyan/shared_data_tmp"
 
-if [ -z "$(ls -A $TARGET_DIR)" ]; then
-    echo "Initializing user environment with default files..."
-    cp -r /tmp/default_data "$TARGET_DIR/data"
-    cp /tmp/default_configs/poc.ipynb "$TARGET_DIR/"
-    cp /tmp/default_configs/settings.ini "$TARGET_DIR/"
+
+mkdir -p "$TARGET_DIR"
+
+
+if [ ! -f "$TARGET_DIR/.initialized" ]; then
+    echo "Copying default Docker files into the mounted workspace..."
+    cp -r /tmp/default_data/. "$TARGET_DIR/"
+    touch "$TARGET_DIR/.initialized"
     chown -R jovyan:users "$TARGET_DIR"
 else
-    echo "User environment already initialized."
+    echo "Docker default files already exist, skipping copy."
+fi
+
+
+if [ -d "$SHARED_TMP" ]; then
+    echo "Creating symlinks for shared read-only data inside $TARGET_DIR..."
+    for item in "$SHARED_TMP"/*; do
+        ln -s "$item" "$TARGET_DIR/$(basename "$item")"
+    done
 fi
 
 exec "$@"
